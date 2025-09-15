671d8db4ad2a3509ba71a62a1e17ac9feb350545
JoanNnadi <nnadijn@mail.uc.edu>
2025-09-15 01:03:26 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(" Enter your monthly credit card balance");
+
+        double initialBalance = 5000; // declare the variables
+        double yearlyInterestRate = 0.17;
+        double monthlyInterestRate = yearlyInterestRate / 12; // value for each month
+        double firstMonth = 0;
+        double secondMonth = 0;
+
+        double firstMonthInterest = initialBalance * yearlyInterestRate;
+        double balanceAfterFirstMonth = initialBalance + firstMonthInterest;
+
+        System.out.println(" your first month interest is: " + firstMonthInterest);
+        System.out.println("your interest after first month is: " + balanceAfterFirstMonth);
+
+
+
+    }
+}
